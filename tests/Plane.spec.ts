b12c5430a7a72bef9f0f6ba06c5b56e7e753120a
Mark Oliinyk <markoliinyk@gmail.com>
2022-07-29 01:36:38 +0300

    Plane: fix tests

--- tests/Plane.spec.ts
+++ tests/Plane.spec.ts
@@ -11,17 +11,17 @@ describe('Plane', () => {
     });
 
     it("intersects with ray that is the front", () => {
-        const ray: Ray = new Ray(new Point(0, 0, -1), new Vector(0, 0, 1));
+        const ray: Ray = new Ray(new Point(0, 0, -1), new Vector(0, 0, 1).normalize());
         expect(plane.intersection(ray)).not.toBeNull();
     });
 
     it("doesn't intersect with parallel ray", () => {
-        const ray: Ray = new Ray(new Point(0, 0, 1), new Vector(0, 1, 0));
+        const ray: Ray = new Ray(new Point(0, 0, 1), new Vector(0, 1, 0).normalize());
         expect(plane.intersection(ray)).toBeNull();
     });
 
     it("doesn't intersect with ray that is behind", () => {
-        const ray: Ray = new Ray(new Point(0, 0, 1), new Vector(0, 1, 0));
+        const ray: Ray = new Ray(new Point(0, 0, 1), new Vector(0, 1, 0).normalize());
         expect(plane.intersection(ray)).toBeNull();
     });
 });
