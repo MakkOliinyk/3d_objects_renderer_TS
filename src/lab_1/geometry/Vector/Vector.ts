2445cabecc6f69e8c892a3fb0b9c619d0d6a89f2
Mark Oliinyk <markoliinyk@gmail.com>
2022-07-29 00:36:15 +0300

    Vector: add basic methods

--- src/lab_1/geometry/Vector/Vector.ts
+++ src/lab_1/geometry/Vector/Vector.ts
@@ -5,7 +5,12 @@ export type TVector = {
 
     length: number;
 
-    normalize: () => TVector;
+    add(vector: TVector): TVector;
+    subtract(vector: TVector): TVector;
+    dot(vector: TVector): number;
+    divide(vector: TVector): TVector;
+    multiply(n: number): TVector;
+    normalize(): TVector;
 }
 
 export class Vector implements TVector {
@@ -25,6 +30,26 @@ export class Vector implements TVector {
             + this.y * this.y
             + this.z * this.z
         );
+    }
+
+    add(vector: Vector): Vector {
+        return new Vector(this.x + vector.x, this.y + vector.y, this.z + vector.z);
+    }
+
+    subtract(vector: Vector): Vector {
+        return new Vector(this.x - vector.x, this.y - vector.y, this.z - vector.z);
+    }
+
+    dot(vector: Vector): number {
+        return this.x * vector.x + this.y * vector.y + this.z * vector.z;
+    }
+
+    divide(vector: Vector): Vector {
+        return new Vector(this.x / vector.x, this.y / vector.y, this.z / vector.z);
+    }
+
+    multiply(n: number): Vector {
+        return new Vector(this.x * n, this.y * n, this.z * n);
     }
 
     normalize(): TVector {
