8c965f047b6e3e385908682dd8f746842b519b60
Mark Oliinyk <markoliinyk@gmail.com>
2022-07-29 00:31:48 +0300

    Vector: add normalize

--- src/lab_1/geometry/Vector/Vector.ts
+++ src/lab_1/geometry/Vector/Vector.ts
@@ -4,6 +4,8 @@ export type TVector = {
     z: number;
 
     length: number;
+
+    normalize: () => TVector;
 }
 
 export class Vector implements TVector {
@@ -23,5 +25,10 @@ export class Vector implements TVector {
             + this.y * this.y
             + this.z * this.z
         );
+    }
+
+    normalize(): TVector {
+        if (this.length === 0) return new Vector(0, 0, 0);
+        return new Vector(this.x / this.length, this.y / this.length, this.z / this.length);
     }
 }
