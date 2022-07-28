56d76939c4654135611c69ad417af3a6e8a8b50c
Mark Oliinyk <markoliinyk@gmail.com>
2022-07-29 01:14:09 +0300

    Plane: add intersection

--- src/lab_1/geometry/Plane/Plane.ts
+++ src/lab_1/geometry/Plane/Plane.ts
@@ -1,9 +1,14 @@
 import { TPoint } from "../Point";
 import { TVector } from "../Vector";
+import { TRay } from "../Ray";
+
+const EPS = 0.00001;
 
 export type TPlane = {
     normal: TVector;
     center: TPoint;
+
+    intersection(ray: TRay): number;
 }
 
 export class Plane implements TPlane {
@@ -13,6 +18,20 @@ export class Plane implements TPlane {
     constructor(normal: TVector, center: TPoint) {
         this.normal = normal;
         this.center = center;
+    }
+
+    // formula: https://bit.ly/2oTe4yj
+    intersection(ray: TRay): number {
+        const d = this.normal.dot(ray.direction);
+
+        if (Math.abs(d) > EPS) {
+            const originCenterNormal: TVector = this.center.subtract(ray.origin).normalize();
+            const t = originCenterNormal.dot(this.normal) / d;
+
+            if (t >= 0) return t;
+        }
+
+        return null;
     }
 }
 
