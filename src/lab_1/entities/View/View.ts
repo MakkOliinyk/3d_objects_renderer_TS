63fd227b1870434fc5bc2ba4f8298d5e4b34ae9e
Mark Oliinyk <markoliinyk@gmail.com>
2022-07-29 14:09:10 +0300

    View: add digest method

--- src/lab_1/entities/View/View.ts
+++ src/lab_1/entities/View/View.ts
@@ -2,6 +2,9 @@ import { Camera } from "../Camera";
 import { ViewMatrix } from "../ViewMatrix";
 import { Light } from "../Light";
 
+import { TPoint } from "../../geometry/Point";
+import { Ray } from "../../geometry/Ray";
+
 export class View {
     camera: Camera;
     matrix: ViewMatrix;
@@ -23,5 +26,39 @@ export class View {
 
     inject(figure: any) {
         this.figures.push(figure);
+    }
+
+    digest() {
+        this.image = '';
+        const origin: TPoint = this.camera.location;
+
+        let resultImage: string = '';
+
+        for (let y = 0; y < this.matrix.height; y++) {
+            for (let x = 0; x < this.matrix.width; x++) {
+                const direction = this.matrix.getValue(x, y).subtract(origin);
+                const ray = new Ray(origin, direction);
+
+                let closestObject: any = null;
+                let closestDistance: number = Infinity;
+
+                for (let obj of this.figures) {
+                    const distance: number = obj.intersection(ray);
+
+                    if (distance !== null && distance < closestDistance) {
+                        closestObject = obj;
+                        closestDistance = distance;
+                    }
+                }
+
+                if (closestObject === null) resultImage += '-'
+                else resultImage += '*';
+
+                resultImage += ' ';
+            }
+            resultImage += '\n';
+        }
+
+        this.image = resultImage;
     }
 }
