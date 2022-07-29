97b1d5fdea4d284cb8f6b8ada0a71674561e0179
Mark Oliinyk <markoliinyk@gmail.com>
2022-07-29 16:13:01 +0300

    lab1: showcase more

--- src/lab_1/index.ts
+++ src/lab_1/index.ts
@@ -13,7 +13,7 @@ const light = new Light(new Vector(1, 1, 1).normalize());
 const matrix = new ViewMatrix(20, 20, 30);
 const view: View = new View(camera, light, matrix);
 
-const plane = new Plane(new Vector(1, 1, 0.3).normalize(), new Point(0, 0, 0));
+const plane = new Plane(new Vector(1, 1, -0.3).normalize(), new Point(0, 0, 0));
 const sphere = new Sphere(new Point(0, 0, 0), 16);
 
 view.inject(plane);
@@ -30,5 +30,15 @@ console.log(view.image);
 view.clear();
 
 view.inject(sphere);
+view.process();
+console.log(view.image);
+
+view.clear();
+
+const sphere_behind: Sphere = new Sphere(new Point(-10,10,-5), 16);
+const sphere_infront: Sphere = new Sphere(new Point(0,0,5), 16);
+
+view.inject(sphere_behind);
+view.inject(sphere_infront);
 view.process();
 console.log(view.image);
