9896c3b8162cd4b6455147790750f9fe6763d1ce
Mark Oliinyk <markoliinyk@gmail.com>
2022-07-29 16:42:16 +0300

    Triangle: add intersection methods

--- src/lab_1/geometry/Triangle/Triangle.ts
+++ src/lab_1/geometry/Triangle/Triangle.ts
@@ -1,9 +1,14 @@
 import { TPoint } from "../Point";
+import { TRay } from "../Ray";
+import { TVector } from "../Vector";
 
 export type TTrinagle = {
     a: TPoint;
     b: TPoint;
     c: TPoint;
+
+    intersect(ray: TRay): number;
+    getPointNormal(_point: TPoint): TVector;
 }
 
 export class Triangle {
@@ -15,5 +20,28 @@ export class Triangle {
         this.a = a;
         this.b = b;
         this.c = c;
+    }
+
+    // formula: https://bit.ly/3oAFbwW
+    intersect(ray: TRay): TPoint {
+        const e1 = this.b.subtract(this.a);
+        const e2 = this.c.subtract(this.a);
+        const p = ray.direction.cross(e2);
+        const a = e1.dot(p);
+        if (a < 0) return null;
+        const f = ray.origin.subtract(this.a);
+        const b = f.dot(p);
+        if (b < 0 || b > a) return null;
+        const d = e1.cross(f).dot(ray.direction);
+        if (d < 0 || d > a) return null;
+        const t = e2.dot(p) / a;
+        return ray.at(t);
+    }
+
+    getPointNormal(_point: TPoint): TPoint {
+        const e1 = this.b.subtract(this.a);
+        const e2 = this.c.subtract(this.a);
+
+        return e1.cross(e2).normalize();
     }
 }
