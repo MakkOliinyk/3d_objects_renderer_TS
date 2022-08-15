cc65137a6e1318fd06fa80eab753fe842c56e056
Mark Oliinyk <markoliinyk@gmail.com>
2022-08-15 13:03:26 +0300

    Figures tests: add more tests

--- tests/Figures.spec.ts
+++ tests/Figures.spec.ts
@@ -7,18 +7,44 @@ describe('Figures', () => {
     let normal: TVector = null;
 
     beforeEach(() => {
-        normal = new Vector(0,0,-1).normalize();
+        normal = new Vector(0, 0, -1).normalize();
     });
 
     it("intersection with both figures but different distance", () => {
-        const sphere_behind = new Sphere(new Point(0, 0, -5), 16);
-        const sphere_infront = new Sphere(new Point(0, 0, 5), 16);
+        const sphere_behind = new Sphere(new Point(0, 0, -5), 10);
+        const sphere_infront = new Sphere(new Point(0, 0, 5), 10);
 
-        const ray = new Ray(new Point(0, 0, 32), normal);
+        const ray = new Ray(new Point(0, 0, 30), normal);
 
         const t_behind = sphere_behind.intersection(ray);
         const t_infront = sphere_infront.intersection(ray);
 
         expect(t_infront < t_behind);
+    });
+
+    it('intersection on edges', () => {
+        const sphere: Sphere = new Sphere(new Point(-10, 0, -5), 10);
+        const otherSphere: Sphere = new Sphere(new Point(10, 0, 5), 10);
+
+        const ray: Ray = new Ray(new Point(0, 0, 30), new Vector(0, 0, -1).normalize());
+
+        const t1 = sphere.intersection(ray);
+        const t2 = otherSphere.intersection(ray);
+
+        expect(t2).not.toBeNull();
+        expect(t1).not.toBeNull();
+    });
+
+    it('no intersection', () => {
+        const sphere: Sphere = new Sphere(new Point(-10, 0, -5), 5);
+        const otherSphere: Sphere = new Sphere(new Point(10, 0, 5), 5);
+
+        const ray: Ray = new Ray(new Point(0, 0, 30), new Vector(0, 0, -1).normalize());
+
+        const t1 = sphere.intersection(ray);
+        const t2 = otherSphere.intersection(ray);
+
+        expect(t2).toBeNull();
+        expect(t1).toBeNull();
     });
 });
