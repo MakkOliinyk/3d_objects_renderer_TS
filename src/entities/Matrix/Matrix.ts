f94450b66619e94742f1e5970da9be6c893ce9a1
Mark Oliinyk <markoliinyk@gmail.com>
2022-08-16 17:57:54 +0300

    Matrix: add multiplication methods

--- src/entities/Matrix/Matrix.ts
+++ src/entities/Matrix/Matrix.ts
@@ -1,3 +1,6 @@
+import { TVector, Vector } from "../../geometry/Vector";
+import { Point, TPoint } from "../../geometry/Point";
+
 const DEFAULT_MATRIX = [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
@@ -12,7 +15,52 @@ export type TMatrix = {
 export class Matrix implements TMatrix {
     data: number[][];
 
+    private multiplyEntity(entity: TVector | TPoint): number[] {
+        const result = [0, 0, 0];
+        const entity_in_array_dimension = [entity.x, entity.y, entity.z];
+
+        for (let i = 0; i < result.length; i++) {
+            for (let j = 0; j < entity_in_array_dimension.length; j++) {
+                result[i] += this.data[i][j] * entity_in_array_dimension[j];
+            }
+        }
+
+        return result;
+    }
+
     constructor(data?: number[][]) {
         this.data = data || DEFAULT_MATRIX;
+    }
+
+    multiply(matrix: Matrix): Matrix {
+        const result = new Matrix();
+
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                let sum = 0;
+
+                for (let k = 0; k < 4; k++) {
+                    sum += this.data[i][k] * matrix.data[k][j];
+                }
+
+                result.data[i][j] = sum;
+            }
+        }
+
+        this.data = result.data;
+
+        return this;
+    }
+
+    multiplyVector(vector: TVector): TVector {
+        const result = this.multiplyEntity(vector);
+
+        return new Vector(result[0], result[1], result[2]);
+    }
+
+    multiplyPoint(point: TPoint): TPoint {
+        const result = this.multiplyEntity(point);
+
+        return new Point(result[0], result[1], result[2]);
     }
 }
